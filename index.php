a66cb631f75917c17c5a3271594b37af21404767
PeterStollenwerk <ps@peterstollenwerk.de>
2020-06-23 20:35:31 +0200

    added Exampleclass

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 
+load([
+  'auf\\plugin\\exampleclass' => 'classes/Exampleclass.php'
+], __DIR__);
+
 Kirby::plugin('auf/plugin', [
     'fields' => [
         'aufpluginfield' => [
           // here we could define the backend logic for our field if needed
-          // let´s write some stuff
         ]
     ]
 ]);
