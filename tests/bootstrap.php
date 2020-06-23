125a5e15be11c254a2bd30563c18caa569d7d226
PeterStollenwerk <ps@peterstollenwerk.de>
2020-06-23 21:48:15 +0200

    cleaned structure

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,12 @@
 
 require_once __DIR__ . '/../kirby/bootstrap.php';
 
+# Set Roots here:
+# https://getkirby.com/docs/reference/system/roots/
+
 new Kirby([
   'roots' => [
-    'content' => __DIR__ . '/kirby/content',
-    'blueprints' => __DIR__ . '/kirby/blueprints'
+    'content' => __DIR__ . '/../content',
+    'blueprints' => __DIR__ . '/../site/blueprints'
   ]
 ]);
