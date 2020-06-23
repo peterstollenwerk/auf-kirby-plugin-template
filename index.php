1e5e85c50ab9f0a9ae2fbeb98464f4791957c611
PeterStollenwerk <ps@peterstollenwerk.de>
2020-06-23 11:20:30 +0200

    changed index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ Kirby::plugin('auf/plugin', [
     'fields' => [
         'aufpluginfield' => [
           // here we could define the backend logic for our field if needed
+          // let´s write some stuff
         ]
     ]
 ]);
